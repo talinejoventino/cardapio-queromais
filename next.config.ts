ad93e39a7233c005cee04536675fd937912e8898
Taline Joventino <taline_peres29@hotmail.com>
2025-08-12 17:17:23 -0300

    refactor: remove unused image configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
